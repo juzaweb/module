fcba7d3b82b5cc8a3bd19bc2ee7a2d8823085c01
Đặng Thế Anh <dangtheanh16@gmail.com>
2021-05-02 10:25:36 +0700

    :truck: Custom autoload

--- config/autoload.php
+++ config/autoload.php
@@ -9,17 +9,20 @@
  * Date: 5/1/2021
  * Time: 4:31 PM
  */
+require __DIR__ .'/../../../vendor/autoload.php';
 $pluginFolders = __DIR__ . '/../../../plugins';
 $pluginFile = __DIR__ . '/../../../bootstrap/cache/plugins_statuses.php';
+
 if (file_exists($pluginFile)) {
-    $plugins = require ($pluginFile);
+    $plugins = require $pluginFile;
     if ($plugins) {
         $loader = new \Composer\Autoload\ClassLoader();
         foreach ($plugins as $pluginInfo) {
             foreach ($pluginInfo as $key => $item) {
                 $path = $pluginFolders . '/' . $item['path'];
-                if (is_dir($path)) {
-                    $loader->setPsr4($item['namespace'], [$path]);
+                $namespace = $item['namespace'] ?? '';
+                if (is_dir($path) && $namespace) {
+                    $loader->setPsr4($namespace, [$path]);
                 }
             }
         }
